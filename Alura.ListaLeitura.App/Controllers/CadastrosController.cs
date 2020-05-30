f0d9da8803456007a97c25b350144ca5bd8bcfe4
douglas souto de souza <douglas_bufao@yahoo.com.br>
2020-05-30 14:17:33 -0300

    Subindo os arquivos

---
+++ Alura.ListaLeitura.App/Controllers/CadastrosController.cs
@@ -0,0 +1,22 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura.ListaLeitura.App.Controllers
+{
+    public class CadastrosController : Controller
+    {
+        public string Incluir(Livro livro)
+        {
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+
+            return "Livro adicionado!";
+        }
+        public IActionResult ExibeFormulario()
+        {          
+            return View("formulario");
+        }
+
+    }
+}
---
+++ Alura.ListaLeitura.App/Controllers/LivrosController.cs
@@ -0,0 +1,53 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.ListaLeitura.App.Controllers
+{
+    public class LivrosController : Controller
+    {
+        public List<Livro> Livros { get; set; }
+
+        public string Detalhes(int id)
+        {
+            var repo = new LivroRepositorioCSV();
+            var livro = repo.Todos.FirstOrDefault(l => l.Id == id);
+
+            if (livro == null)
+            {
+                return "Livro não encontrado";
+            }
+
+            return livro.Detalhes();
+        }
+        public IActionResult ParaLer()
+        {
+            var repo = new LivroRepositorioCSV();
+            ViewBag.Livros = repo.ParaLer.Livros;
+
+            return View("lista");
+        }
+        public IActionResult Lidos()
+        {
+            var repo = new LivroRepositorioCSV();
+            ViewBag.Livros = repo.Lidos.Livros;
+
+            return View("lista");
+        }
+        public IActionResult Lendo()
+        {
+            var repo = new LivroRepositorioCSV();
+            ViewBag.Livros = repo.Lendo.Livros;
+
+            return View("lista");
+        }
+
+        public string Teste()
+        {
+            return "Testando o roteamento do framework mvc";
+        }
+
+    }
+}
